d71e700e06436c86d2d2a1ba5bd0bcf590ab0ce4
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2013-02-16 21:39:48 +0200

    Implementing type-safe game logic

--- src/hs/Main.hs
+++ src/hs/Main.hs
@@ -1,5 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE Rank2Types #-}
 
 
 import Network.Wai (Application)
@@ -10,6 +19,8 @@ import WaiAppStatic.Types (MaxAge(..))
 
 import Network.Wai.Handler.WebSockets (intercept)
 import qualified Network.WebSockets as WS
+
+import Data.Maybe (fromMaybe)
 
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -23,17 +34,22 @@ import Network.WebSockets.Messaging
 
 import GHC.Generics (Generic)
 
-import Control.Monad (void, liftM2, guard)
-
+import Control.Applicative ((<|>),(<$>))
+import Control.Monad (void, liftM2, guard, join)
 import Control.Concurrent (forkIO)
-
 import Control.Concurrent.STM
+
+import Unsafe.Coerce
 
 data NameNotification = Name String deriving Generic
 
 data ServerRequest
     = AskName
+    | AskMove
     | GameBoard Board
+    | WonGame
+    | LostGame
+    | DrawGame
     deriving Generic
 
 data Board = Board (Map Position Piece)
@@ -42,6 +58,61 @@ data Coord = Coord Int deriving (Eq, Ord)
 
 type Position = (Coord, Coord)
 data Piece = X | O
+
+type family Other (p :: Piece) :: Piece
+type instance Other X = O
+type instance Other O = X
+
+newtype Move (piece :: Piece) = Move Position deriving (FromJSON, ToJSON)
+
+data User (piece :: Maybe Piece) = User
+    { userName :: String
+    , userConn :: Connection
+    }
+
+data Game turn = Game
+    { gameBoard :: Board
+    , gameState :: GameState turn
+    }
+
+data GameState (turn :: Piece) where
+    Turn  :: (Move turn -> Maybe (Game (Other turn))) -> GameState turn
+    Draw  :: GameState a
+    Win   :: Piece -> GameState a
+
+type NewPlayer    = User Nothing
+type Player piece = User (Just piece)
+
+foldGameState
+    :: ((Move turn -> Maybe (Game (Other turn))) -> r)
+    -> r
+    -> (Piece -> r)
+    -> GameState turn
+    -> r
+foldGameState handleTurn handleDraw handleWin s = case s of
+    Turn f -> handleTurn f
+    Draw   -> handleDraw
+    Win p  -> handleWin p
+
+getMove :: Player piece -> IO (Future (Move piece))
+getMove (User{..}) = requestAsync userConn AskMove
+
+assignSides :: NewPlayer -> NewPlayer -> (Player X, Player O)
+assignSides pl1 pl2 = (unsafeCoerce pl1, unsafeCoerce pl2)
+
+newGame :: Game X
+newGame = Game newBoard $ Turn $ go X newBoard where
+
+    go :: Piece -> Board -> Move turn -> Maybe (Game (Other turn))
+    go p (Board mp) (Move pos)
+        | pos `Map.member` mp = Nothing
+        | otherwise = Just $ Game board' $ fromMaybe (Turn $ go p' board') gameOver where
+            board' = Board $ Map.insert pos p mp
+            gameOver = Nothing
+            p' = case p of
+                O -> X
+                X -> O
+
 
 instance ToJSON Coord where
     toJSON (Coord i) = toJSON i
@@ -95,9 +166,10 @@ initWSApp = do
     return $ \req -> do
         WS.acceptRequest req
         onConnect $ \conn -> do
+            name <- request conn AskName
             atomically $ do
                 notify conn $ GameBoard newBoard
-                writeTChan queue conn
+                writeTChan queue $ User name conn
 
             return ()
 
@@ -108,18 +180,38 @@ newBoard = Board $ Map.fromList
     ]
 -- newBoard = Board $ Map.empty
 
-matchMaker :: TChan Connection -> IO ()
+matchMaker :: TChan NewPlayer -> IO ()
 matchMaker queue = do
     (p1,p2) <- atomically $ liftM2 (,) (readTChan queue) (readTChan queue)
-    return ()
+    void $ forkIO $ playGame $ assignSides p1 p2
 
-nextConnected :: TChan Connection -> STM Connection
+nextConnected :: TChan NewPlayer -> STM NewPlayer
 nextConnected queue = do
-    conn <- readTChan queue
-    disc <- readTVar $ disconnected conn
+    u@User{..} <- readTChan queue
+    disc <- readTVar $ disconnected userConn
     if disc
         then nextConnected queue
-        else return conn
+        else return u
+
+playGame :: (Player X, Player O) -> IO ()
+playGame (px, po) = go px po newGame where
+
+    go :: (Other t ~ t', Other t' ~ t) => Player t -> Player t' -> Game t -> IO ()
+    go p p' (Game b t) = sendBoard >> foldGameState turn draw win t where
+        turn f =
+            let loop       = getMove p >>= join . atomically . foldFuture disco nextTurn
+                disco      = atomically $ notify (userConn p') WonGame
+                nextTurn m = maybe loop (go p' p) (f m)
+            in  loop
+
+        draw = undefined
+
+        win _ = undefined
+
+        sendBoard = atomically $ do
+            notify (userConn p)  (GameBoard b)
+            notify (userConn p') (GameBoard b)
+
 
 main :: IO ()
 main = do
