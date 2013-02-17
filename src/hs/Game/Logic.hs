ec6d09191eb8d72ebe3d45bff50d35c51fbfda8a
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2013-02-17 13:41:34 +0200

    Check that move is within board and code cleanup

--- src/hs/Game/Logic.hs
+++ src/hs/Game/Logic.hs
@@ -7,7 +7,7 @@
 
 module Game.Logic (playGame, assignSides) where
 
-import Control.Applicative ((<|>))
+import Control.Applicative ((<|>), (<$>))
 import Control.Monad (msum, void, join, when)
 import Control.Concurrent.STM (TChan, atomically, writeTChan)
 import Control.Concurrent (forkIO)
@@ -33,9 +33,6 @@ type CyclicMove turn =
     , MoveAssoc (Other turn)
     )
 
-(<?>) :: Maybe a -> a -> a
-(<?>) = flip fromMaybe
-
 maybeIf :: Bool -> a -> Maybe a
 maybeIf p a
     | p         = Just a
@@ -51,29 +48,36 @@ stripSide :: Player t -> NewPlayer
 stripSide = unsafeCoerce
 
 newGame :: Game X
-newGame = Game newBoard $ Turn $ go newBoard where
+newGame = Game newBoard $ Turn $ makeMove newBoard where
 
-    go :: CyclicMove turn => Board -> Move turn -> Maybe (Game (Other turn))
-    go (Board mp) move
-        | pos `Map.member` mp = Nothing
-        | otherwise = Just $ Game board' $ gameOver <?> Turn (go board')
-        where
-            assoc@(pos, piece) = moveAssoc move
-            board'   = Board mp'
-            mp'      = uncurry Map.insert assoc mp
-            gameOver = victory <|> draw
-            draw     = maybeIf (Map.size mp' == 9) Draw
-            victory  = fullRow <|> fullCol <|> fullDiag
-            fullRow  = msum [match (1, r) (1, 0) | r <- [1..3]]
-            fullCol  = msum [match (c, 1) (0, 1) | c <- [1..3]]
-            fullDiag = match (1, 1) (1, 1) <|> match (3, 1) (-1, 1)
-            match (sx, sy) (dx, dy) = go' (2 :: Int) sx sy where
-                go' 0 _ _ = Just $ Win piece
-                go' n x y
-                    | lookup' (x, y) == Just piece = go' (n-1) (x+dx) (y+dy)
-                    | otherwise                   = Nothing
+makeMove :: CyclicMove turn => Board -> Move turn -> Maybe (Game (Other turn))
+makeMove (Board mp) move
+    | pos `Map.member` mp = Nothing
+    | x < 1 || x > 3 || y < 1 || y > 3 = Nothing
+    | otherwise = Game board' <$> (gameOver <|> nextTurn)
+    where
+        assoc@(pos, piece) = moveAssoc move
+        (Coord x, Coord y) = pos
 
-            lookup' (x,y) = Map.lookup (Coord x, Coord y) mp'
+        board'   = Board mp'
+        mp'      = uncurry Map.insert assoc mp
+
+        nextTurn = Just $ Turn $ makeMove board'
+        gameOver = victory <|> draw
+        draw     = maybeIf (Map.size mp' == 9) Draw
+        victory  = fullRow <|> fullColumn <|> fullDiagonal
+
+        fullRow      = msum [match (1, r) (1, 0) | r <- [1..3]]
+        fullColumn   = msum [match (c, 1) (0, 1) | c <- [1..3]]
+        fullDiagonal = match (1, 1) (1, 1) <|> match (3, 1) (-1, 1)
+
+        match (sx, sy) (dx, dy) = go (2 :: Int) sx sy where
+            go 0 _ _ = Just $ Win piece
+            go n x y
+                | lookup' (x, y) == Just piece = go (n-1) (x+dx) (y+dy)
+                | otherwise                    = Nothing
+
+        lookup' (x,y) = Map.lookup (Coord x, Coord y) mp'
 
 newBoard :: Board
 newBoard = Board $ Map.fromList
@@ -92,11 +96,11 @@ playGame queue (px, po) = start >> play >> both requeue where
     play = go px po newGame
 
     go :: Cyclic t t' => Player t -> Player t' -> Game t -> IO ()
-    go p p' (Game b t) = sendBoard >> foldGameStatus turn draw win t where
+    go p p' (Game b st) = sendBoard >> foldGameStatus turn draw win st where
         turn f = loop where
             loop        = getMove p >>= resolveMove
-            resolveMove = join . atomically . foldFuture disco nextTurn
-            disco       = atomically $ notify (userConn p') WonGame
+            resolveMove = join . atomically . foldFuture disconnect nextTurn
+            disconnect  = atomically $ notify (userConn p') WonGame
             nextTurn m  = maybe loop (go p' p) (f m)
 
         draw = atomically $ both $ \u -> notify (userConn u) DrawGame
@@ -105,7 +109,7 @@ playGame queue (px, po) = start >> play >> both requeue where
             notify (userConn p) LostGame
             notify (userConn p') WonGame
 
-        sendBoard = atomically $ both $ \u -> notify (userConn u)  (GameBoard b)
+        sendBoard = atomically $ both $ \u -> notify (userConn u) (GameBoard b)
 
     both :: Monad m => (forall t.Player t -> m ()) -> m ()
     both op = op px >> op po
